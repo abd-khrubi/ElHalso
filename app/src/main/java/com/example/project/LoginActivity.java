c23b8b9b66d847caf6c4d458dd5a23122882f0f7
fouadhal <fouad.hallomi@mail.huji.ac.il>
2020-09-09 13:20:59 +0300

    first

---
+++ app/src/main/java/com/example/project/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}kl
