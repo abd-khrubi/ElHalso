c896c2a02bafa0f0bd60bbacd253de32eafcf12e
fouadhal <fouad.hallomi@mail.huji.ac.il>
2020-09-10 21:45:59 +0300

    adding email-pass login

--- app/src/main/java/com/example/project/LoginActivity.java
+++ app/src/main/java/com/example/project/LoginActivity.java
@@ -18,6 +18,8 @@ import com.facebook.appevents.AppEventsLogger;
 import com.facebook.internal.WebDialog;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -33,6 +35,9 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
+import java.util.Arrays;
+import java.util.List;
+
 
 public class LoginActivity extends AppCompatActivity {
     private CallbackManager mCallerbackManager;
@@ -40,6 +45,7 @@ public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth mFirebaseAuth;
     private static final String TAG = "LoginAuthentication";
     private static final int RC_GOOGLE_SIGN_IN = 17;
+    private static final int RC_SIGN_IN = 914;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,11 +56,29 @@ public class LoginActivity extends AppCompatActivity {
         mFirebaseAuth = FirebaseAuth.getInstance();
         setupFacebookLogin();
         setupGoogleLogin();
+
+
+
+//        {
+//            // Choose authentication providers
+//            List<AuthUI.IdpConfig> providers = Arrays.asList(
+//                    new AuthUI.IdpConfig.EmailBuilder().build(),
+//                    new AuthUI.IdpConfig.GoogleBuilder().build(),
+//                    new AuthUI.IdpConfig.FacebookBuilder().build());
+//
+//// Create and launch sign-in intent
+//            startActivityForResult(
+//                    AuthUI.getInstance()
+//                            .createSignInIntentBuilder()
+//                            .setAvailableProviders(providers)
+//                            .build(),
+//                    RC_SIGN_IN);
+//        }
     }
 
     private void setupGoogleLogin() {
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_google_web_client_id))
+                .requestIdToken(getString(R.string.google_oauth2_client_id))
                 .requestEmail()
                 .build();
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
@@ -111,7 +135,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        mCallerbackManager.onActivityResult(requestCode, resultCode, data);
+//        mCallerbackManager.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == RC_GOOGLE_SIGN_IN) {
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
@@ -123,6 +147,22 @@ public class LoginActivity extends AppCompatActivity {
             } catch (ApiException e) {
                 // Google Sign In failed, update UI appropriately
                 Log.d(TAG, "Google sign in failed", e);
+            }
+        }
+        else if(requestCode == RC_SIGN_IN){
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                Log.d(TAG, "S U C C E S S");
+                Log.d(TAG, user.toString());
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
             }
         }
     }
