51fda44c735493b5820ccfcf3a931ef9e15dc706
fouadhal <fouad.hallomi@mail.huji.ac.il>
2020-09-10 13:37:51 +0300

    creating google sign in

--- app/src/main/java/com/example/project/LoginActivity.java
+++ app/src/main/java/com/example/project/LoginActivity.java
@@ -1,17 +1,110 @@
 package com.example.project;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
+import com.facebook.internal.WebDialog;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class LoginActivity extends AppCompatActivity {
+    private CallbackManager mCallerbackManager;
+    private FirebaseAuth mFirebaseAuth;
+    private static final String TAG = "LoginAuthentication";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Log.d(TAG, "OnCreate");
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        setupFacebookLogin();
+        setupGoogleLogin();
     }
-}kl
+
+    private void setupGoogleLogin() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken()
+                .requestEmail()
+                .build();
+        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+//        already signed in
+//        GoogleSignInAccount googleAcc = GoogleSignIn.getLastSignedInAccount(this);
+//        if(googleAcc != null){
+//
+//        }
+
+    }
+
+    private void setupFacebookLogin() {
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
+        mCallerbackManager = CallbackManager.Factory.create();
+        LoginButton fbLoginBtn = (LoginButton) findViewById(R.id.fbLoginBtn);
+        fbLoginBtn.registerCallback(mCallerbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "Successfully authenticated to Facebook");
+                handleFacebookToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "Cancel authenticating to Facebook");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "Error authenticating to Facebook");
+            }
+        });
+    }
+
+    private void handleFacebookToken(AccessToken token){
+        Log.d(TAG, "Handling Facebook token");
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mFirebaseAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Log.d(TAG, "Sign in to Firebase with credential successful");
+                    FirebaseUser user = mFirebaseAuth.getCurrentUser();
+                }
+                else{
+                    Log.d(TAG, "Sign in to Firebase with credential failed");
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mCallerbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+}
