0d6887feeaf4755f8dbf15356aae18ba2c75a35f
fouadhal <63809210+fouadhal@users.noreply.github.com>
2020-09-13 19:24:23 +0300

    finishing up email login

--- app/src/main/java/com/example/project/EmailLoginActivity.java
+++ app/src/main/java/com/example/project/EmailLoginActivity.java
@@ -1,14 +1,152 @@
 package com.example.project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
 
 public class EmailLoginActivity extends AppCompatActivity {
 
+    private boolean signupMode;
+    private static final String EMAIL_REGEX = "^(.+)@(.+)\\.(.+)$";
+    private static final String TAG = "EmailSign";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_email_login);
+        signupMode = true;
+        changeSignMode(findViewById(R.id.signinBtn));
+    }
+
+    public void changeSignMode(View v){
+        boolean toSignupMode = v.getId() == R.id.signupBtn;
+        if(signupMode == toSignupMode)
+            return;
+
+        signupMode = toSignupMode;
+        TextView emailTxt = (TextView) findViewById(R.id.emailTxt);
+        TextView nameTxt = (TextView) findViewById(R.id.nameTxt);
+        TextView passTxt = (TextView) findViewById(R.id.passTxt);
+        TextView pass2Txt = (TextView) findViewById(R.id.pass2Txt);
+        Button signBtn = (Button) findViewById(R.id.signBtn);
+
+        nameTxt.setVisibility(signupMode ? View.VISIBLE : View.GONE);
+        pass2Txt.setVisibility(signupMode ? View.VISIBLE : View.GONE);
+
+        ConstraintLayout layout = findViewById(R.id.emailLayout);
+        ConstraintSet set = new ConstraintSet();
+        if(signupMode){
+            set.connect(nameTxt.getId(), ConstraintSet.TOP, emailTxt.getId(), ConstraintSet.BOTTOM, 20);
+            set.connect(passTxt.getId(), ConstraintSet.TOP, nameTxt.getId(), ConstraintSet.BOTTOM, 20);
+            set.connect(pass2Txt.getId(), ConstraintSet.TOP, passTxt.getId(), ConstraintSet.BOTTOM, 20);
+            set.connect(signBtn.getId(), ConstraintSet.TOP, pass2Txt.getId(), ConstraintSet.BOTTOM, 20);
+            signBtn.setText("Sign up");
+        }
+        else {
+            set.connect(passTxt.getId(), ConstraintSet.TOP, emailTxt.getId(), ConstraintSet.BOTTOM, 20);
+            set.connect(signBtn.getId(), ConstraintSet.TOP, passTxt.getId(), ConstraintSet.BOTTOM, 20);
+            signBtn.setText("Sign in");
+        }
+    }
+
+    private boolean validateDetails(){
+        TextView emailTxt = (TextView) findViewById(R.id.emailTxt);
+        TextView nameTxt = (TextView) findViewById(R.id.nameTxt);
+        TextView passTxt = (TextView) findViewById(R.id.passTxt);
+        TextView pass2Txt = (TextView) findViewById(R.id.pass2Txt);
+
+        Pattern pattern = Pattern.compile(EMAIL_REGEX);
+        Matcher matcher = pattern.matcher(emailTxt.getText().toString());
+        if(!matcher.matches()){
+            showMessage("Email address is invalid.");
+            return false;
+        }
+
+        if(passTxt.getText().toString().length() < 6){
+            showMessage("Password length is too short.");
+            return false;
+        }
+        if(signupMode && !pass2Txt.getText().toString().equals(passTxt.getText().toString())){
+            showMessage("Passwords do not match.");
+            return false;
+        }
+        if(signupMode && nameTxt.getText().toString().length() < 3){
+            showMessage("Name is too short.");
+            return false;
+        }
+
+        return true;
+    }
+
+    private void showMessage(String msg){
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    public void signMethod(View v){
+        if(!validateDetails())
+            return;
+
+        String email = ((EditText)findViewById(R.id.emailTxt)).getText().toString();
+        String pass = ((EditText)findViewById(R.id.emailTxt)).getText().toString();
+        final FirebaseAuth mFirebaseAuth = FirebaseAuth.getInstance();
+
+        if(signupMode){
+            mFirebaseAuth.createUserWithEmailAndPassword(email, pass)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Log.d(TAG, "Sign up success");
+                                Intent intent = new Intent();
+                                setResult(Activity.RESULT_OK, intent);
+                                finish();
+                            } else {
+                                Log.d(TAG, "Authentication failed", task.getException());
+                                showMessage("Sign up failed. Please try again later.");
+                            }
+
+                        }
+                    });
+        }
+        else {
+            mFirebaseAuth.signInWithEmailAndPassword(email, pass)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Log.d(TAG, "Authentication success");
+                                Intent intent = new Intent();
+                                setResult(Activity.RESULT_OK, intent);
+                                finish();
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.d(TAG, "Authentication failed", task.getException());
+                                showMessage("Authentication failed!");
+                            }
+                        }
+                    });
+        }
+
     }
 }
--- app/src/main/java/com/example/project/LoginActivity.java
+++ app/src/main/java/com/example/project/LoginActivity.java
@@ -49,7 +49,8 @@ public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth mFirebaseAuth;
     private static final String TAG = "LoginAuthentication";
     private boolean isBusinessLogin;
-    private static final int RC_GOOGLE_SIGN_IN = 17;
+    private static final int RC_GOOGLE_SIGN_IN = 901;
+    private static final int RC_EMAIL_SIGN_IN = 902;
     private static final int RC_SIGN_IN = 914;
     private static final int BUSINESS_LOGO_ID = R.drawable.common_google_signin_btn_icon_disabled;
     private static final int REGULAR_LOGO_ID = R.drawable.common_google_signin_btn_icon_dark;
@@ -59,29 +60,16 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Log.d(TAG, "OnCreate");
         mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseAuth.signOut();
         setupFacebookLogin();
         setupGoogleLogin();
         isBusinessLogin = false;
+    }
 
-
-
-//        {
-//            // Choose authentication providers
-//            List<AuthUI.IdpConfig> providers = Arrays.asList(
-//                    new AuthUI.IdpConfig.EmailBuilder().build(),
-//                    new AuthUI.IdpConfig.GoogleBuilder().build(),
-//                    new AuthUI.IdpConfig.FacebookBuilder().build());
-//
-//// Create and launch sign-in intent
-//            startActivityForResult(
-//                    AuthUI.getInstance()
-//                            .createSignInIntentBuilder()
-//                            .setAvailableProviders(providers)
-//                            .build(),
-//                    RC_SIGN_IN);
-//        }
+    public void emailLogin(View v){
+        Intent intent = new Intent(this, EmailLoginActivity.class);
+        startActivityForResult(intent, RC_EMAIL_SIGN_IN);
     }
 
     public void showTypesDialog(View view){
@@ -122,11 +110,9 @@ public class LoginActivity extends AppCompatActivity {
         this.isBusinessLogin = isBusiness;
         if(isBusiness){
             logoImg.setImageResource(BUSINESS_LOGO_ID);
-            Log.d(TAG, "setting business");
         }
         else {
             logoImg.setImageResource(REGULAR_LOGO_ID);
-            Log.d(TAG, "setting regular");
         }
     }
 
@@ -189,7 +175,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-//        mCallerbackManager.onActivityResult(requestCode, resultCode, data);
+        mCallerbackManager.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == RC_GOOGLE_SIGN_IN) {
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
@@ -203,22 +189,19 @@ public class LoginActivity extends AppCompatActivity {
                 Log.d(TAG, "Google sign in failed", e);
             }
         }
-//        else if(requestCode == RC_SIGN_IN){
-//            IdpResponse response = IdpResponse.fromResultIntent(data);
-//
-//            if (resultCode == RESULT_OK) {
-//                // Successfully signed in
-//                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-//                Log.d(TAG, "S U C C E S S");
-//                Log.d(TAG, user.toString());
-//                // ...
-//            } else {
-//                // Sign in failed. If response is null the user canceled the
-//                // sign-in flow using the back button. Otherwise check
-//                // response.getError().getErrorCode() and handle the error.
-//                // ...
-//            }
-//        }
+        else if(requestCode == RC_EMAIL_SIGN_IN){
+            Log.d(TAG, "signed in with email");
+            Log.d(TAG, FirebaseAuth.getInstance().getCurrentUser().getUid());
+            if(FirebaseAuth.getInstance().getCurrentUser().getDisplayName() == null){
+                Log.d(TAG, "user name is null");
+                return;
+            }
+            else {
+                Log.d(TAG, FirebaseAuth.getInstance().getCurrentUser().getDisplayName());
+                Log.d(TAG, FirebaseAuth.getInstance().getCurrentUser().getEmail());
+                Log.d(TAG, FirebaseAuth.getInstance().getCurrentUser().getPhoneNumber());
+            }
+        }
     }
 
     private void firebaseAuthWithGoogle(String idToken) {
