49fddf1f84a817e2e9f6cf0b67438ee960ec1f3a
fouadhal <63809210+fouadhal@users.noreply.github.com>
2020-09-14 20:02:33 +0300

    init firebase handler class

---
+++ app/src/main/java/com/example/project/FirebaseHandler.java
@@ -0,0 +1,56 @@
+package com.example.project;
+
+import android.location.Location;
+
+import java.util.ArrayList;
+
+public class FirebaseHandler {
+
+    public Business getBusinessForUser(String UID) {
+
+    }
+
+    public ArrayList<Review> getReviewsForBusiness(String UID) {
+
+    }
+
+    public ArrayList<Business> getBusinessNear(Location loc){
+
+    }
+
+    public void updateBusiness(Business newBusiness){
+
+    }
+
+    public void addImageToBusinessGallery(String UID, ImageURL image){
+
+    }
+
+    public void deleteImageForBusiness(String UID, ImageURL image){
+
+    }
+
+    public void addReview(User user, Business business) {
+
+    }
+
+    public void deleteReview(Review rev){
+
+    }
+
+    public User getUser(String UID) {
+
+    }
+
+    public void updateUser(User newUser) {
+
+    }
+
+    public void addFavoriteBusiness(String UID, Business business) {
+
+    }
+
+    public void removeFavoriteBusiness(String UID, Business business) {
+
+    }
+}
